5eef50b46f6c92a04a66f939cb656c936cc390d2
Yao Min <ngyaomin@ymail.com>
2017-05-22 01:42:31 +0800

    Add div tag to about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,9 +2,12 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <h3>About page</h3>
-    <p>welcome to about page</p>
+    <div>
+      <h3>About page</h3>
+      <p>welcome to about page</p>
+    </div>
   )
+
 };
 
 module.exports = About;
