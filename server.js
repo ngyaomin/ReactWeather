1228900961b905403265906a17007195d2925d96
Yao Min <ngyaomin@ymail.com>
2017-05-22 01:06:01 +0800

    Debug syntax error

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express();
-const PORT = process.env.PORT; || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next){
   if (req.headers['x-forwarded-proto'] === 'http') {
