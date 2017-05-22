19baf60b87e4d8d83952a15062539947a96892a0
Yao Min <ngyaomin@ymail.com>
2017-05-22 20:04:39 +0800

    Style about page, add links

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,17 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About page</h3>
-      <p>welcome to about page</p>
+      <h1 className='text-center'>About page</h1>
+      <p>A simple weather application build with react</p>
+      <p>Some Tools used</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a>
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - External weather API
+        </li>
+      </ul>
     </div>
   )
 
